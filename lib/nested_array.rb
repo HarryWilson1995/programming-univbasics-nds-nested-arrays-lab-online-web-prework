69714b0711a43c787bdc3a84bcd58b543829e15a
Harry Wilson <harry.wilson1995a@gmail.com>
2019-11-22 15:05:45 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -21,8 +21,8 @@ def assembled_matrix
   # and the Enfield Tennis Academy as provided by the constants
   residents = [
     ["Don Gately","Joelle van Dyne", "Pat M.", "Kate Gompert", "Bruce Green"],
-    
-    
+    ["Hal Incandenza","Lyle","Gerhard Schtitt","Mario Incandenza","Michael Pemulis"]
+    ]
 end
 
 def array_literal_matrix
